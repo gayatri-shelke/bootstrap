8a1d48da7ba147612ebb9c8a872954743d4eebdf
gayatri <gayatrisanjayshelke20@navgurukul.org>
2022-05-31 18:28:14 +0530

    first commit

---
+++ g.py
@@ -0,0 +1,139 @@
+# import requests
+# url=requests.get('https://www.youtube.com/watch?v=orBQesFBkXg&t=6s')
+
+# print(url.text)
+# .section-head{
+#     margin-left: 60%;
+# }
+
+# .heading-one{
+#     margin-top: 350px;
+#     margin-left: 40%;
+#     color: black;
+#     font-size: 40px;
+# }
+# .engine{
+#     width: 50px;
+#     height: 50px;
+#     border: 10px solid #00e5ff;
+# }
+# .tyres{
+#     width: 50px;
+#     height: 50px;
+#     /* margin-left: 20%; */
+#     border: 10px solid #00e5ff;
+# }
+# .tranmission{
+#     width: 50px;
+#     height: 50px;
+#     /* text-align: center; */
+#     /* margin-left: 20%; */
+#     border: 10px solid #00e5ff;
+# }
+# .section-head{
+#     color: black;
+#     /* text-align: center; */
+# }
+# .engine_para{
+#     color: black;
+#     /* text-align: center; */
+# }
+
+# .section-head_1{
+#     color: black;
+#     /* text-align: center; */
+# }
+# .tyres_para{
+#     color: black;
+#     /* text-align: center; */
+# }
+
+
+# .section-head_2{
+#     color: black;
+#     /* text-align: center; */
+# }
+# .trasmission_para{
+#     color: black;
+#     /* text-align: center; */
+# }
+
+
+
+
+# <div class="container section">
+    #     <div class="row">
+    #         <div class="col-md-12">
+    #             <h2 class="heading-one">OUR SERVICES</h2>
+    #         </div>
+    #         <div class="col-md-4">
+    #              <div class="service-box">
+    #                 <img class="engine" src="turbo.png">
+    #                 <h2 class="section-head">Engine Repair</h2>
+    #                 <p class="engine_para">we fix engine of your vehicle at your doorsteps</p>
+    #                 </div>
+    #             </div>
+
+    #         </div>
+    #         <div class="col-md-4">
+    #             <div class="service-box">
+    #                 <img class="tyres" src="logo.png">
+    #                 <h2 class="section-head_1">Tyres Replacement</h2>
+    #                 <p class="tyres_para">we replace tyres of your vehicle at your doorsteps</p>
+    #                 </div>
+    #         </div>
+    #         <div class="col-md-4">
+    #             <div class="service-box">
+    #                 <img class="tranmission" src="logo1.png">
+    #                 <h2 class="section-head_2">Transmission</h2>
+    #                 <p class="trasmission_para">we fix the tranmission of your vehicle at your doorsteps</p>
+    #             </div>
+    #         </div>
+            
+    #     </div>
+    # </div>
+
+
+
+# a="9"
+# b="9"
+# print(a+b)
+
+# try:
+#   i=int(input('enter a number'))
+#   c=1/i
+#   Except Exception as e:
+#       print(e)
+#   else:
+#     print('we were succesful')
+
+
+# def increment(num):
+#   try:
+#     return int(num)+1
+#   except:
+#     reise valueError('this is not good')
+
+
+  
+
+
+date =int(input("enter date"))
+month=int(input("enter month"))
+year=int(input("enter year"))
+if date>=1 and month==month and year==year:
+      print(1+date,month,year)
+elif date>=1 or date<=28 and month==2 and year%4==0:
+      print(date+1,month+1,year)
+
+
+
+
+
+
+
+
+
+
+
+  
